50e3b847c486279b38faf30f086afb665134a8f8
Matúš Brliť <matus.brlit@gmail.com>
2015-02-28 22:44:23 +0100

    use MongoDB, register new users
    fixes #2, fixes #3

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var hbs = require('hbs');
+var mongoose = require('mongoose');
+var bodyparser = require('body-parser');
 
+var models = require('./models.js');
 var config = require('./config.js');
 
 console.log("Starting...");
@@ -13,13 +16,32 @@ var app = express();
 app.set('view engine', 'html');
 app.engine('html', hbs.__express);
 app.use(express.static('static'));
-app.get('/', function(req, res) {
+app.use(bodyparser.json());
+
+// serve the app
+app.get('/', function (req, res) {
     res.setHeader('Content-Type', 'text/html; charset=UTF-8');
     res.setHeader('Content-Language', 'sk');
-    
+
     res.render('index.html');
+});
+
+// AJAX calls
+app.post('/users/register', function (req) {
+    var user = new models.User();
+    user.email = req.body.email;
+    user.password = req.body.password;
+    user.register();
 });
 console.log("Done");
 
 app.listen(config.port);
 console.log('Listening on port ' + config.port + '...');
+
+console.log("Connecting database...");
+mongoose.connect('mongodb://localhost:27017/ERNIcinema');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    console.log("Done");
+});
---
+++ models.js
@@ -0,0 +1,38 @@
+﻿var mongoose = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator');
+
+var userSchema = mongoose.Schema({
+    name: String,
+    password: String,
+    email: { type: 'string', unique: true }
+});
+
+userSchema.path('email').index({ unique: true });
+
+userSchema.pre("save", function (next) {
+    var self = this;
+    mongoose.models["User"].findOne({ email: self.email }, function (err, user) {
+        if (err) {
+            next(err);
+        } else if (user) {
+            self.invalidate("email", "email must be unique");
+            next(new Error("email must be unique"));
+        } else {
+            next();
+        }
+    });
+});
+
+userSchema.methods.register = function () {
+    var self = this;
+    this.save(function (err) {
+        if (err) {
+            console.log(err);
+            return err;
+        } else {
+            console.log("Saved: " + JSON.stringify(self));
+        }
+    });
+};
+
+exports.User = mongoose.model('User', userSchema);
